9d1905b5e3587a714c396c06e9e52188693f250e
nadine-padernal <60122574+nadine-padernal@users.noreply.github.com>
2020-02-13 11:06:34 +0800

    Update rss-Fnews.php

--- rss-Fnews.php
+++ rss-Fnews.php
@@ -9,14 +9,23 @@
 				$title = $data->getElementsByTagName("title")->item(0)->nodeValue;
 				$link = $data->getElementsByTagName("link")->item(0)->nodeValue;
 				$desc = $data->getElementsByTagName("description")->item(0)->nodeValue;
-				$image = $data->getElementsByTagName("name")->item(0)->nodeValue;
 				$pub = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
 			echo "<div><ul class = 'page'>
-			<h1><li><a href = '$link' target = '_blank' class = 'anchor'>'$title'</a></li></h1></ul>
+			<h1 ><li><a href = '$link' target = '_blank' class = 'anchor'>'$title'</a></li></h1></ul>
 				<ul class = 'topic'>
 				<h2><li>$desc</li></h2>
-				<li><a href = '$image' target = '_blank' class = 'anchor'>$image</a></li>
 				<h4><li>$pub</li></h4>
 				</ul></div>";
 	}
 			?>
+			<style>
+			h1{
+			color: #000;
+			}
+			h2{
+			color: #000;
+			}
+			h4{
+			color: #000;
+			}
+			</style>
